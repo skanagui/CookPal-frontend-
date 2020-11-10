28315bb79631e3e0c26e63e705575f8f353070c9
jayelle0 <you@example.com>
2020-11-09 22:50:43 -0500

    api test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,51 @@
 console.log('hello')
+const recipeListUl = document.querySelector('#recipe-list')
+
+
+fetch('https://api.spoonacular.com/recipes/complexSearch?query=italian&number=10&instructionsRequired=true&apiKey=b0d50cace88e4214aab6bf3c2bc47dac')
+  .then(response => response.json())
+  .then(recipeArray => {
+    renderRecipeList(recipeArray.results)
+      console.log(recipeArray.results)
+    });
+
+// debugger
+    
+    recipeListUl.innerHTML = ""
+
+    const renderRecipeList = (recipeArray) => {
+        // recipeListUl.innerHTML = ""
+        recipeArray.forEach(renderARecipe)
+    }
+
+    const renderARecipe = (recipe) => {
+        const recipeLi = document.createElement("li")
+        recipeLi.innerText= recipe.title
+        recipeLi.dataset.id= recipe.id
+        recipeListUl.append(recipeLi)
+  }
+
+  const renderBody = (recipeItem) => {
+    const recipeLi = document.createElement("li")
+    recipeLi.innerText= recipe.title
+    recipeLi.dataset.id= recipe.id
+    recipeListUl.append(recipeLi)
+}
+
+
+  recipeListUl.addEventListener('click', (event) => {
+    console.log(event.target)  
+    let id = event.target.dataset.id
+    let idNum = parseInt(id)
+    console.log(typeof idNum, idNum)  
+
+
+    fetch(`https://api.spoonacular.com/recipes/{idNum}/information&apiKey=b0d50cace88e4214aab6bf3c2bc47dac`)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    });
+
+  })
+
+
